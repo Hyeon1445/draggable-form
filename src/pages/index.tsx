998517eba420943bca01837813501b6f37fd34ae
hyeon1445 <lily1445@gmail.com>
2023-05-04 01:27:02 +0900

    feat: implement card container style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,29 +23,32 @@ const SimpleDndExample = () => {
   }
 
   return (
-    <DragDropContext onDragEnd={handleOnDragEnd}>
-      <Droppable droppableId="list">
-        {(provided) => (
-          <ul {...provided.droppableProps} ref={provided.innerRef}>
-            {list.map((item, index) => (
-              <Draggable key={item} draggableId={item} index={index}>
-                {(provided) => (
-                  <Card
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    style={provided.draggableProps.style}
-                  >
-                    {item}
-                  </Card>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <>
+      <Title>Draggable Cards</Title>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="list">
+          {(provided) => (
+            <CardContainer {...provided.droppableProps} ref={provided.innerRef}>
+              {list.map((item, index) => (
+                <Draggable key={item} draggableId={item} index={index}>
+                  {(provided) => (
+                    <Card
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={provided.draggableProps.style}
+                    >
+                      {item}
+                    </Card>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </CardContainer>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </>
   )
 }
 
@@ -54,7 +57,26 @@ const Card = styled.li`
   cursor: pointer;
   height: 3rem;
   width: 10rem;
-  background-color: yellow;
+  background-color: teal;
+  color: white;
+`
+
+const CardContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin: 1.5rem;
+  background-color: #f2f2f2;
+  padding: 1rem;
+  list-style: none;
+  width: 13rem;
+`
+
+const Title = styled.p`
+  color: teal;
+  font-weight: bold;
+  font-size: 14px;
+  padding: 1.5rem 1.5rem 0;
 `
 
 export default SimpleDndExample
